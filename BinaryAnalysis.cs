58809c744be96b515c14215900c4d826837f1d23
wzy-99 <lhwzy99@163.com>
2021-11-28 22:56:25 +0800

    v1.4
    添加不定长分帧策略

--- BinaryAnalysis.cs
+++ BinaryAnalysis.cs
@@ -10,11 +10,11 @@ namespace binary_analysis_tool
     public class BinaryAnalysis
     {
         [DefaultCommand,
-        Command(Name = "framingwriting",
-        Usage = "framingwriting <string> <string>",
-        Description = "frame and write binary file",
+        Command(Name = "fbl",
+        Usage = "fbl <string> <string> <int> <byte[]> [int] [int]",
+        Description = "framing by length and write binary file",
         ExtendedHelpText = "more details and examples")]
-        public void FramingWriting(
+        public void FramingByLength(
             [Option(LongName = "inputFile", ShortName = "i",
                     Description = "the path of input file")]
             string inputFile,
@@ -39,7 +39,54 @@ namespace binary_analysis_tool
             IReadStrategy readStrategy = new FileReadStrategy(inputFile, length, cycle);
             ReadBlock readBlock = new ReadBlock(readStrategy);
 
-            FramingStrategy framingStrategy = new FramingStrategy(candidate, frameLength);
+            IFramingStrategy framingStrategy = new FramingLengthStrategy(candidate, frameLength);
+            FramingBlock framingBlock = new FramingBlock(framingStrategy);
+
+            IWriteStrategy writeStrategy = new FileWriteStrategy(outputFile);
+            WriteBlock writeBlock = new WriteBlock(writeStrategy);
+
+            framingBlock.SetAction((input) =>
+            {
+                writeBlock.Post(input);
+            });
+
+            readBlock.SetAction((input) =>
+            {
+                framingBlock.Post(input);
+            });
+
+            readBlock.Start();
+
+            Log.Information("Finish");
+            Console.ReadLine();
+        }
+        [Command(Name = "fbh",
+        Usage = "fbh <string> <string> <byte[]> [int] [int]",
+        Description = "framing by head and write binary file",
+        ExtendedHelpText = "more details and examples")]
+        public void FramingByHead(
+            [Option(LongName = "inputFile", ShortName = "i",
+                    Description = "the path of input file")]
+            string inputFile,
+            [Option(LongName = "outputFile", ShortName = "o",
+                    Description = "the path of output file")]
+            string outputFile,
+            [Option(LongName = "candidate", ShortName = "c",
+                    Description = "the candidate bytes")]
+            byte[] candidate,
+            [Option(LongName = "length", ShortName = "l",
+                    Description = "the length of read block, default is 1024")]
+            int length=1024,
+            [Option(LongName = "cycle", ShortName = "t",
+                    Description = "the times of reading， 0 for reading all the file")]
+            int cycle=0)
+        {
+            InitLog();
+
+            IReadStrategy readStrategy = new FileReadStrategy(inputFile, length, cycle);
+            ReadBlock readBlock = new ReadBlock(readStrategy);
+
+            IFramingStrategy framingStrategy = new FramingHeadStrategy(candidate);
             FramingBlock framingBlock = new FramingBlock(framingStrategy);
 
             IWriteStrategy writeStrategy = new FileWriteStrategy(outputFile);
--- FramingStrategy.cs
+++ FramingStrategy.cs
@@ -11,13 +11,13 @@ namespace binary_analysis_tool
 
     }
 
-    public class FramingStrategy : IFramingStrategy
+    public class FramingLengthStrategy : IFramingStrategy
     {
         private byte[] _buffer;
         private byte[] _candidate;
         private int _length;
         private Action<Byte[]> dataArrived;
-        public FramingStrategy(byte[] candidate, int length)
+        public FramingLengthStrategy(byte[] candidate, int length)
         {
             _candidate = candidate;
             _length = length;
@@ -58,6 +58,107 @@ namespace binary_analysis_tool
                 byte[] new_buffer = new byte[_buffer.Length - i];
                 Array.Copy(_buffer, i, new_buffer, 0, _buffer.Length - i);
                 _buffer = new_buffer;
+            }
+        }
+        private bool IsMatch(byte[] array, int position)
+        {
+            for (int i = 0; i < _candidate.Length; i++)
+                if (array[position + i] != _candidate[i])
+                    return false;
+
+            return true;
+        }
+        public void SetAction(Action<Byte[]> action)
+        {
+            dataArrived = action;
+        }
+
+    }
+
+    public class FramingHeadStrategy : IFramingStrategy
+    {
+        private byte[] _buffer;
+        private byte[] _candidate;
+        private int _length;
+        private int _second_index;
+        private int _first_index;
+        private bool _find_flag;
+        private Action<Byte[]> dataArrived;
+        public FramingHeadStrategy(byte[] candidate)
+        {
+            _candidate = candidate;
+            _length = candidate.Length;
+            _second_index = 0;
+            _first_index = 0;
+            _find_flag = false;
+        }
+        public void Framing(Byte[] bytes)
+        {
+            // 将到来的bytes储放至_buffer
+            if (_buffer == null)
+            {
+                _buffer = bytes;
+            }
+            else
+            {
+                _buffer = _buffer.Concat(bytes).ToArray();
+            }
+
+            // 如果_buffer中的长度大于最小数据帧长度
+            if (_buffer.Length >= _length)
+            {
+                int i = 0;
+                if (_find_flag)
+                {
+                    i = _first_index;
+                }
+                else
+                {
+                    i = _second_index;
+                }
+
+                for (; i <= _buffer.Length - _length; i++)
+                {
+                    if (!IsMatch(_buffer, i))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        if (_find_flag) // 搜索第二个head
+                        {
+                            // 取定长bytes作为一帧返回
+                            _second_index = i;
+                            byte[] ret = new byte[_second_index - _first_index];
+                            Array.Copy(_buffer, _first_index, ret, 0, _second_index - _first_index);
+                            _first_index = _second_index;
+                            Log.Verbose("Success frame {Bytes}", ret);
+                            dataArrived?.Invoke(ret);
+                        }
+                        else // 搜索第一个head
+                        {
+                            _find_flag = true;
+                            _first_index = i;
+                            _second_index = i;
+                        }
+                    }
+                }
+               
+                // 删除_buffer内已经被处理的字节
+                if (_find_flag)
+                {
+                    byte[] new_buffer = new byte[_buffer.Length - _first_index];
+                    Array.Copy(_buffer, _first_index, new_buffer, 0, _buffer.Length - _first_index);
+                    _buffer = new_buffer;
+                    _second_index = _second_index - _first_index;
+                    _first_index = 0;
+                }
+                else
+                {
+                    byte[] new_buffer = new byte[_buffer.Length - _length];
+                    Array.Copy(_buffer, _buffer.Length - _length, new_buffer, 0, _length);
+                    _buffer = new_buffer;
+                }
             }
         }
         private bool IsMatch(byte[] array, int position)
